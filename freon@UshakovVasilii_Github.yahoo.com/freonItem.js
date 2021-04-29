isDipesh/gnome-shell-extension-sensors

commit 3029f4f7b8740e078b5cc7cf2776627c110ab572

fix "Error meta is null on Gnome 3.38"

freon@UshakovVasilii_Github.yahoo.com/freonItem.js
@@ -1,6 +1,7 @@
 const GObject = imports.gi.GObject;
 const PopupMenu = imports.ui.popupMenu;
 const St = imports.gi.St;
+const Clutter = imports.gi.Clutter;
 
 var FreonItem = GObject.registerClass(class FreonItem extends PopupMenu.PopupBaseMenuItem {
 
@@ -10,9 +11,9 @@ var FreonItem = GObject.registerClass(class FreonItem extends PopupMenu.PopupBas
         this._key = key;
         this._gIcon = gIcon;
 
-        this._labelActor = new St.Label({text: displayName ? displayName : label});
+        this._labelActor = new St.Label({text: displayName ? displayName : label, x_align: Clutter.ActorAlign.CENTER, x_expand: true});
         this.actor.add(new St.Icon({ style_class: 'popup-menu-icon', gicon : gIcon}));
-        this.actor.add(this._labelActor, {x_fill: true, expand: true});
+        this.actor.add_child(this._labelActor);
         this._valueLabel = new St.Label({text: value});
         this.actor.add(this._valueLabel);
     }

jonasmalacofilho/gnome-shell-extension-freon

commit 13052bac4d2189421ea0606a1d12e1e5ab86a161

Change back to default/left aligned items
Besides the fix for GNOME 3.38, commit 3029f4f7b874 ('fix "Error meta is
null on Gnome 3.38"') also changed the alignment of the freon items.

Change back to default/left aligned items, since they look a lot better.

Fixes: 3029f4f7b874 ('fix "Error meta is null on Gnome 3.38"')

freon@UshakovVasilii_Github.yahoo.com/freonItem.js
@@ -11,7 +11,7 @@ var FreonItem = GObject.registerClass(class FreonItem extends PopupMenu.PopupBas
         this._key = key;
         this._gIcon = gIcon;
 
-        this._labelActor = new St.Label({text: displayName ? displayName : label, x_align: Clutter.ActorAlign.CENTER, x_expand: true});
+        this._labelActor = new St.Label({text: displayName ? displayName : label, x_expand: true});
         this.actor.add(new St.Icon({ style_class: 'popup-menu-icon', gicon : gIcon}));
         this.actor.add_child(this._labelActor);
         this._valueLabel = new St.Label({text: value});